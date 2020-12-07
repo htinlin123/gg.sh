e409d71cba6dcfb2ccc9cb3fe5f581f6578823d4
htinlin123 <75575889+htinlin123@users.noreply.github.com>
2020-12-07 13:31:56 +0630

    Update and rename README.md to gg.sh

---
+++ gg.sh
@@ -0,0 +1,31 @@
+#!/data/data/com.termux/files/usr/bin/bash
+echo Hello! Welcome From My Script .I Am Not A Pr>
+echo Enjoy | lolcat
+pkg upgrade | lolcat
+pkg upgrade -y | lolcat
+pkg install python -y | lolcat
+pkg install python2 -y | lolcat
+pkg install figlet -y | lolcat
+pkg install wget -y | lolcat
+pkg install php -y | lolcat
+pkg install ruby -y | lolcat
+pkg install libffi -y | lolcat
+pkg install libgmp -y| lolcat
+pkg install libpcap -y | lolcat
+pkg install postgresql -y | lolcat
+pkg install readline -y | lolcat
+pkg install libsqlite -y | lolcat
+pkg install openssl -y | lolcat
+pkg install libxml2 -y | lolcat
+pkg install libxslt -y | lolcat
+pkg install ncurses -y | lolcat
+pkg install libgrpc -y | lolcat
+clear
+figlet Htin | lolcat
+figlet Lin | lolcat
+echo script made by
+toilet Htin | lolcat
+toilet Lin | lolcat
+toilet Aung |lolcat
+echo script made by ထင္လင္းေအာင္😎 | lolcat
+cd $home
